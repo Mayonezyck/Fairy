6f2ae40d8c16227ff77b41810ceab4c5735ce9e3
Mayonezyck <yicheng_zhu@outlook.com>
2024-07-10 00:26:54 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,8 +20,11 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Define the function to be called when the bot is mentioned
-def mentioned_function(message_content):
-    print(f"Bot was mentioned! Message: {message_content}")
+async def mentioned_function(message):
+    cleaned_message = re.sub(r'<@!?[0-9]+>', '', message.content).strip()
+    response = f"Bot was mentioned! Message: {cleaned_message}"
+    await message.channel.send(response)
+
 
 # Event that triggers when the bot is ready
 @bot.event
@@ -31,13 +34,21 @@ async def on_ready():
 # Event that triggers on new message
 @bot.event
 async def on_message(message):
+    #Debug
+    if message.author == bot.user:
+        return
+    print('on message')
+    line1 = f'MESSAGEAUTHOR:{message.author},BOTUSER{bot.user}'
+    await message.channel.send(line1)
+
+    ifMention = f'BOT MENTIONED?{bot.user.mentioned_in(message)}'
+    await message.channel.send(ifMention)
     # Check if the message mentions the bot
     if bot.user.mentioned_in(message):
         # Remove the mention part from the message content
-        cleaned_message = re.sub(r'<@!?[0-9]+>', '', message.content).strip()
-        mentioned_function(cleaned_message)
+        await mentioned_function(message)
     # Process commands if any
-    await bot.process_commands(message)
+    #await bot.process_commands(message)
 
 # Start the bot
 bot.run(TOKEN)
