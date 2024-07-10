36fac3faf421a5d449b7ea87b6912761fe52c7b5
Mayonezyck <yicheng_zhu@outlook.com>
2024-07-09 23:55:54 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,43 @@
 import yaml
 import discord
 from discord.ext import commands
+import re
 
 def load_secret():
     with open("secret.yaml", "r") as f:
         secret = yaml.safe_load(f)
-        return secret.get('DISCORD', default)
+        return secret.get('DISCORD')
 
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+TOKEN = load_secret()
+
+# Intents are required to read messages
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
-Discord_token = load_secret
-# Define the bot and its command prefix
-bot = commands.Bot(command_prefix='!')
 
-# Event listener for when the bot has connected to the server
+# Creating bot instance with command prefix '!'
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Define the function to be called when the bot is mentioned
+def mentioned_function(message_content):
+    print(f"Bot was mentioned! Message: {message_content}")
+
+# Event that triggers when the bot is ready
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Bot is ready. Logged in as {bot.user}')
 
-# Event listener for when a new member joins the server
+# Event that triggers on new message
 @bot.event
-async def on_member_join(member):
-    channel = discord.utils.get(member.guild.text_channels, name='general')
-    if channel:
-        await channel.send(f'Welcome to the server, {member.mention}!')
+async def on_message(message):
+    # Check if the message mentions the bot
+    if bot.user.mentioned_in(message):
+        # Remove the mention part from the message content
+        cleaned_message = re.sub(r'<@!?[0-9]+>', '', message.content).strip()
+        mentioned_function(cleaned_message)
+    # Process commands if any
+    await bot.process_commands(message)
 
-# Command for the bot to respond to
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello!')
-
-# Run the bot with your token
-
-bot.run(load_secret())
+# Start the bot
+bot.run(TOKEN)
